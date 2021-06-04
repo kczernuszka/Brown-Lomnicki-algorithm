9dc6650d6861bc73fd03811a9d80d4f0cbeca934
kczernuszka <kczernuszka@o2.pl>
2021-06-04 18:39:38 +0200

    Parse the table values

--- scripts/script.js
+++ scripts/script.js
@@ -4,16 +4,14 @@ function inputsAreSet() {
 }
 
 function createTable() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const rows = urlParams.get('strategies_a');
-  const columns = urlParams.get('strategies_b');
+  const params = getUrlParams();
   table = document.createElement('table');
   table.style.width = '100px';
   table.style.border = '1px solid black';
 
-  for (let i = 0; i < rows; i++) {
+  for (let i = 0; i < params.rows; i++) {
     let tr = table.insertRow();
-    for (let j = 0; j < columns; j++) {
+    for (let j = 0; j < params.columns; j++) {
       let td = tr.insertCell();
       let input = document.createElement('input');
       input.setAttribute("id", `input${i}_${j}`);
@@ -23,4 +21,45 @@ function createTable() {
     }
   }
   document.getElementById("table").appendChild(table);
+}
+
+function getTableValues() {
+  params = getUrlParams();
+  let matrix = [];
+  for (let i = 0; i < params.rows; i++) {
+    let columns_in_row = [];
+    for (let j = 0; j < params.columns; j++) {
+      cell = document.getElementById(`input${i}_${j}`).value;
+      if (isNaN(cell) || cell === "") return [];
+      columns_in_row.push(parseInt(cell));
+    }
+    matrix.push(columns_in_row);
+  }
+  return matrix;
+}
+
+function getUrlParams() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const rows = urlParams.get('strategies_a');
+  const columns = urlParams.get('strategies_b');
+  return {rows, columns}
+}
+
+function showResult() {
+  let matrix = getTableValues();
+  if(matrix.length !== 0) {
+    runAlgorithm();
+  }
+  else {
+    printError();
+  }
+}
+
+function runAlgorithm() {
+
+}
+
+function printError() {
+  document.getElementById("error").innerHTML = 
+      "<p style=\"color:red\">Incorrect values in the table!</p>";
 }
